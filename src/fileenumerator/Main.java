bdb51d2a5c8a2b7d4d9942370e53418ef5daa7d2
Su-Shing Chen <ssc55@uclive.ac.nz>
2015-05-22 22:30:45 +1200

    Set up the controller. Can drag in files.

--- src/fileenumerator/Main.java
+++ src/fileenumerator/Main.java
@@ -1,23 +1,41 @@
 package fileenumerator;
 
+import fileenumerator.gui.MainPaneController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+/**
+ * Main application to run. Load the view and pass over control to the controller.
+ */
 public class Main extends Application {
 
-  @Override
-  public void start(Stage primaryStage) throws Exception {
-    Parent root = FXMLLoader.load(getClass().getResource("gui/MainPane.fxml"));
-    primaryStage.setTitle("File Enumerator");
-    primaryStage.setScene(new Scene(root, 300, 275));
-    primaryStage.show();
-  }
-
-
+  /**
+   * Main method to launch application.
+   * @param args Command line arguments
+   */
   public static void main(String[] args) {
     launch(args);
+  }
+
+  /**
+   * Launch the application.
+   * @param primaryStage The primary stage
+   * @throws Exception Something went wrong or something?
+   */
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    FXMLLoader loader = new FXMLLoader(getClass().getResource("gui/MainPane.fxml"));
+    Parent root = loader.load();
+    MainPaneController mainPaneController = loader.getController();
+
+    primaryStage.setTitle("File Enumerator");
+    primaryStage.setScene(new Scene(root, 300, 275));
+
+    mainPaneController.setupController(primaryStage);
+
+    primaryStage.show();
   }
 }
--- src/fileenumerator/gui/MainPaneController.java
+++ src/fileenumerator/gui/MainPaneController.java
@@ -1,4 +1,48 @@
 package fileenumerator.gui;
 
+import java.io.File;
+
+import javafx.scene.Scene;
+import javafx.scene.input.Dragboard;
+import javafx.scene.input.TransferMode;
+import javafx.stage.Stage;
+
+/**
+ * Main controller for the GUI.
+ */
 public class MainPaneController {
+
+  /**
+   * Set up the GUI to have a minimum size and behaviour on file drag.
+   * @param stage The primary stage
+   */
+  public void setupController(Stage stage) {
+
+    stage.setMinHeight(480);
+    stage.setMinWidth(640);
+
+    Scene scene = stage.getScene();
+
+    scene.setOnDragOver(event -> {
+      Dragboard dragboard = event.getDragboard();
+      if (dragboard.hasFiles()) {
+        event.acceptTransferModes(TransferMode.COPY);
+      } else {
+        event.consume();
+      }
+    });
+
+    scene.setOnDragDropped(event -> {
+      Dragboard dragboard = event.getDragboard();
+      if (dragboard.hasFiles()) {
+        String filePath = null;
+        for (File file : dragboard.getFiles()) {
+          filePath = file.getAbsolutePath();
+          System.out.println(filePath);
+        }
+        event.setDropCompleted(true);
+        event.consume();
+      }
+    });
+  }
 }
